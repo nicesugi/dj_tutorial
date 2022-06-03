b21645aa6a4f341d1cd57ad944aeb3355f5a84dd
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-03 09:33:24 +0900

    Feat Django 템플렛언어와 for,if문으로 저녁메뉴 랜덤고르기

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 
@@ -6,5 +7,14 @@ def index(request):
     return render(request, 'index.html')
 
 
-def dinner(request):
-    return render(request, 'dinner.html')
+def dinner(request, name):
+    menus = [{'name':'족발', 'price':30000}, {'name':'햄버거', 'price':5000}, {'name':'치킨', 'price':20000}, {'name':'초밥','price':15000}]
+    pick = random.choice(menus)
+    context = {
+        'pick': pick,
+        'name': name,
+        'menus':menus,
+        
+    }
+    
+    return render(request, 'dinner.html', context)
